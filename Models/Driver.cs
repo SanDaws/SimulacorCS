19a00ca83feca08e0f5ad16e3d93d28c03a6cdcc
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-02 11:21:05 -0500

    Feat: LicenseCategory

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -9,15 +9,36 @@ public class Driver:User
     //atributes
     public string LicenesNumber;
     public string LicenseCategory;
-    public string DrivingExperience;
+    public int DrivingExperience;
 
     //methods
     //cosntructor
 
-    public Driver(string name,string lastName,string typeDocument,string identificationNumber,string email,string phoneNumber,string Adreess,DateOnly bday,string licenesNumber,string licenseCategory, string drivingExperience ):
+    public Driver(string name,string lastName,string typeDocument,string identificationNumber,string email,string phoneNumber,string Adreess,DateOnly bday,string licenesNumber,string licenseCategory, int drivingExperience ):
     base(name,lastName,typeDocument,identificationNumber,email,phoneNumber,Adreess,bday){
     LicenesNumber=licenesNumber;
     LicenseCategory=licenseCategory;
     DrivingExperience=drivingExperience;
     }
+public static string LicenseCategoryKind(){
+    Console.WriteLine($@"
+    Categoria de Licensia:
+    1: A2
+    2: B2");
+   ConsoleKeyInfo response= Console.ReadKey();
+    switch (response.Key)
+    {
+        case ConsoleKey.D1:
+        return "A2";
+        case ConsoleKey.D2:
+        return "B2";
+        default:
+        Util.RedText("opcion no valida o existente");
+        LicenseCategoryKind();
+        break;
+    }
+    return "A2";
 }
+}
+
+
