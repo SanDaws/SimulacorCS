61c0ac230976e1f1133bbb7853f8c22abca6f574
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-02 07:48:47 -0500

    fix: moderm namespace call

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Simulacro.Models
+namespace Simulacro.Models;
+public class Customer:User
 {
-    public class Customer:User
-    {
-        //attributes
-        public string MembershipLevel;
-        public string PreferedPaymentMethod;
-        // i will pass and create a exception [SafeDateOnyConvertor] to avoid non existing dates
-        public Customer(string name,string lastName,string typeDocument,string identificationNumber,string membershipLevel,string email,string preferedPaymentMethod,string phoneNumber,string Adreess,DateOnly bday ):
-        base(name,lastName,typeDocument,identificationNumber,email,phoneNumber,Adreess,bday){
+    //attributes
+    public string MembershipLevel;
+    public string PreferedPaymentMethod;
+    // i will pass and create a exception [SafeDateOnyConvertor] to avoid non existing dates
+    public Customer(string name,string lastName,string typeDocument,string identificationNumber,string membershipLevel,string email,string preferedPaymentMethod,string phoneNumber,string Adreess,DateOnly bday ):
+    base(name,lastName,typeDocument,identificationNumber,email,phoneNumber,Adreess,bday){
 
-            MembershipLevel=membershipLevel;
-            PreferedPaymentMethod=preferedPaymentMethod;
-
-        }
-        public void UpdateMembershipLevel(){}//ask how to implement
-
+        MembershipLevel=membershipLevel;
+        PreferedPaymentMethod=preferedPaymentMethod;
 
     }
+    public void UpdateMembershipLevel(){}//ask how to implement
+
+
 }
